b610dc5e39c2be854e6392d1bf0c4bf5cad2f12f
George <43950567+Hurstiii@users.noreply.github.com>
2021-02-26 16:19:24 +0000

    initial commit
    Setup a react-app typescript template and an example tornado server.

---
+++ Back-end/app.py
@@ -0,0 +1,23 @@
+import tornado.ioloop
+import tornado.web
+import gym
+
+# env = FrozenLake() # instantiate the default environment.
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello World")
+
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler)
+    ])
+
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
+    print("Now listening on localhost:8888")
